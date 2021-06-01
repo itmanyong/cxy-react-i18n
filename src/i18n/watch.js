cd093671b57308ba642eae36eb1ff98a372d8a87
cxy <itmanyong@gmail.com>
2021-06-01 17:59:31 +0800

    z
    z

--- src/i18n/watch.js
+++ src/i18n/watch.js
@@ -1,30 +1,27 @@
-import { STORE_LANG_KEY } from '../configs/vars';
+import { MODEL_NAME, STORE_LANG_KEY } from '../configs/vars';
+import { dispatch } from 'concent';
+
+// 延迟到下一个事件循环执行reducer函数，确保watch流程中的改变数据先落地，之后触发的另外一轮computed函数
+const invokeReducer = (reducer, payload) => setTimeout(() => dispatch(reducer, payload), 0);
+
 /**
  * 根据当前lang的存在性重置lang
  */
 export const watch_lang_message = {
 	fn: ({ lang, message }, o, f) => {
-		if (lang && !message[lang] && f.cuVal.i18nLangKeys[0]) {
-			f.refCtx.mr.setLang(f.cuVal.i18nLangKeys[0]);
+		if (!lang && Object.keys(message)[0]) {
+			const setLang = f.refCtx.reducer[MODEL_NAME].setLang;
+			if (localStorage.getItem(STORE_LANG_KEY)) {
+				invokeReducer(setLang, localStorage.getItem(STORE_LANG_KEY));
+			} else {
+				invokeReducer(setLang, Object.keys(message)[0]);
+			}
+		}
+		if (lang && lang != o.lang) {
+			localStorage.setItem(STORE_LANG_KEY, lang);
 		}
 	},
 	depKeys: ['lang', 'message'],
-	compare: true,
-	immediate: true,
-};
-/**
- * 根据lang变化存储本地
- */
-export const watch_lang = {
-	fn: ({ lang }, o, f) => {
-		if (lang) {
-			console.log(lang);
-			localStorage.setItem(STORE_LANG_KEY, lang);
-		} else {
-			localStorage.removeItem(STORE_LANG_KEY);
-		}
-	},
-	depKeys: ['lang'],
 	compare: true,
 	immediate: true,
 };
